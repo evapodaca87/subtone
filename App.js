15e86eaf568e05d17173de332efb9bbf5b3fc85b
evapodaca87 <evapodaca87@gmail.com>
2019-01-12 18:19:19 -0700

    Revert "changhed epilepsy warning duration"
    This reverts commit bcd25a1d5034cdd1d2721cd2382f4d588bcc2bf1.

--- App.js
+++ App.js
@@ -28,7 +28,7 @@ export default class App extends Component {
                 color: 'red',
                 textAlign: 'center'
             },
-            duration: 1000
+            duration: 2000
         });
     };
 
